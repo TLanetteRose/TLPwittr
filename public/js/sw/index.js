c21e7047425e8ae35ebb5b88ba210f52768e5fe3
T. Lanette <TLPollard4516@gmail.com>
2019-08-18 00:45:15 -0400

    App files

---
+++ public/js/sw/index.js
@@ -0,0 +1,42 @@
+var staticCachName = 'wittr-static-v2';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+    // Ensure at least $primary-color changes
+    // DONE: change cache name to 'wittr-static-v2'
+    caches.open('wittr-static-v2').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    // DONE: remove the old cache
+    caches.keys().then(function(cacheNames) {
+    	return Promise.all(
+    		cacheNames.filter(function(cacheName) {
+    			return cacheName.startsWith('wittr-') && cacheName != staticCachName;
+    		}).map(function(cacheName) {
+    			return caches.delete(cacheName);
+    		})
+    	);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
